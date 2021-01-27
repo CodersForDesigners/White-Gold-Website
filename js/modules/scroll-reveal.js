467a5df99114e45e64eb4d8869945c0c926d7ab6
adi <52442230+adi-lazaro@users.noreply.github.com>
2021-01-27 11:22:44 +0530

    Scroll Reveal: Account for scrolling layers that have a `position` of `fixed`

--- js/modules/scroll-reveal.js
+++ js/modules/scroll-reveal.js
@@ -6,6 +6,7 @@ $( function ( $ ) {
 	 * Store values for reference
 	 *
 	 */
+
 	// DOM elements
 	var $elementsToReveal = Array.prototype.slice.call( $( ".scroll-reveal" ) )
 							.map( function ( el ) {
@@ -13,6 +14,23 @@ $( function ( $ ) {
 							} );
 	// User Coordinates
 	var currentScrollTop;
+		// ...that are relative to the...
+	var currentScrollingLayer = null;
+
+	// Setup the scroll handler
+	layoutNavigation();
+	setupScrollRevealHandler();
+
+	// Switch up the scroll handlers when the scrolling layer changes
+	$( document ).on( "scrolling-layer/change", function ( event, data ) {
+		if ( $elementsToReveal.length === 0 )
+			return;
+
+		removeScrollRevealHandler( currentScrollingLayer );
+		setupScrollRevealHandler( data.domNode );
+		currentScrollingLayer = data.domNode || null;
+		layoutNavigation();
+	} );
 
 
 	// Returns whether or not an element has **not** been "revealed",
@@ -23,28 +41,50 @@ $( function ( $ ) {
 
 	function layoutNavigation () {
 
+		if ( $elementsToReveal.length === 0 )
+			removeScrollRevealHandler();
+
 		var viewportHeight = $( window ).height();
-		currentScrollTop = window.scrollY || document.body.scrollTop;
+		if ( currentScrollingLayer )
+			currentScrollTop = currentScrollingLayer.scrollTop
+		else
+			currentScrollTop = window.pageYOffset || window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
 
 		/*
 		 * Reveal elements as they are scrolled to
 		 */
+		var viewportBottom = currentScrollTop + ( 0.75 * viewportHeight );
+		// var viewportBottom = currentScrollTop + viewportHeight;
 		for ( let _i = 0; _i < $elementsToReveal.length; _i += 1 ) {
-			if (
-				$elementsToReveal[ _i ].offset().top
-					<
-				currentScrollTop + ( 0.85 * viewportHeight )
-			) {
+			// If the element is hidden because it or its ancestor has their `display` property set to `none`
+			if ( ! $elementsToReveal[ _i ].get( 0 ).offsetParent )
+				continue;
+			var elementTop = $elementsToReveal[ _i ].get( 0 ).offsetTop;
+			// var elementTop = $elementsToReveal[ _i ].offset().top;
+			if ( elementTop < viewportBottom ) {
 				$elementsToReveal[ _i ]
 					.addClass( "reveal" )
-					.trigger( "reveal/post" )
+					// .trigger( "reveal/post" )
 			}
 		}
 		$elementsToReveal = $elementsToReveal.filter( hasElementNotBeenRevealed );
 
 	}
 
-	layoutNavigation();
-	$( window ).on( "scroll", layoutNavigation );
+	function setupScrollRevealHandler ( scrollingLayer ) {
+		scrollingLayer = scrollingLayer || null;
+
+		if ( scrollingLayer )
+			$( scrollingLayer ).on( "scroll", layoutNavigation );
+		else
+			$( window ).on( "scroll", layoutNavigation );
+	}
+
+	function removeScrollRevealHandler ( scrollingLayer ) {
+		if ( scrollingLayer )
+			$( scrollingLayer ).off( "scroll", layoutNavigation );
+		else
+			$( window ).off( "scroll", layoutNavigation );
+	}
 
 } );
